ac0fe9d7e5b015784a71114aac3ba01ec589a6d1
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2026-01-12 00:37:55 +0530

    Add initial implementations for classes and promises; refactor sum function

---
+++ javascript/classes.js
@@ -0,0 +1,29 @@
+// // classes in js
+// // rectangle class
+
+// class Rectangle {
+//     constructor(length, width) {
+//         this.length = length
+//         this.width = width
+//     }
+//     area(){
+//         return this.length*this.width;
+        
+//     }
+// }
+// const rectangle = new Rectangle(2,4)
+// const area = rectangle.area()
+// console.log(area);
+
+//################################################################################
+
+//Date class
+// const date = new Date()
+// console.log(date.getFullYear())
+
+const user = new Map()
+user.set("name","Tarun")
+user.set("age", 23)
+console.log(user.get("name"));
+
+
--- javascript/tempCodeRunnerFile.js
+++ javascript/tempCodeRunnerFile.js
@@ -1,9 +1,10 @@
-function sum(num){
-//     let count =0
-//     for(let i = 0; i <=num; i++ ){
-//         count = count + i
-//     }
-//    return count
-// }
-// let ans = sum(5)
-// console.log(ans)
+function sum(num) {
+  let total = 0;
+  for (let i = 0; i <= num; i++) {
+    total += i;
+  }
+  return total;
+}
+
+const ans = sum(5);
+console.log(ans);
